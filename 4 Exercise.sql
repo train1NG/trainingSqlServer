6e31d5bcaf9d5ed7fa71f88b94faa9829bc45bc2
train1NG <tentiparfeb14@gmail.com>
2024-12-15 19:57:27 +0700

    4. Temporary table and common table expression

---
+++ 4 Exercise.sql
@@ -0,0 +1,8 @@
+1.Buatlah sebuah CTE untuk menghitung total penjualan bulanan dari tabel Sales dan mencari bulan dengan total penjualan tertinggi. Tampilkan kolom:
+Bulan
+Total Penjualan Bulanan
+Indikasi apakah bulan tersebut memiliki penjualan tertinggi ("Ya" atau "Tidak").
+
+
+2.Buatlah sebuah query untuk menghitung total penjualan per karyawan, termasuk hanya karyawan yang memiliki penjualan lebih besar dari rata-rata semua penjualan.
+

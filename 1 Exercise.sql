de66d35201c30798d4ba95ff2fd760e14eb4f3c0
train1NG <tentiparfeb14@gmail.com>
2024-12-15 19:23:27 +0700

    Update 1 Exercise.sql

--- 1 Exercise.sql
+++ 1 Exercise.sql
@@ -13,25 +13,10 @@ Soal:
 Tampilkan TanggalTransaksi dalam format DD-MMM-YYYY HH:MM:SS AM/PM dan hitung jumlah hari hingga tanggal saat ini.
 
 
-SELECT 
-    FORMAT(TanggalTransaksi, 'dd-MMM-yyyy hh:mm:ss tt') AS TanggalTransaksiFormat,
-    DATEDIFF(DAY, TanggalTransaksi, GETDATE()) AS HariSejakTransaksi
-FROM Transaksi
-WHERE DATEDIFF(DAY, TanggalTransaksi, GETDATE()) > 90;
-
-
 3. Kalkulasi dengan Fungsi Waktu
 Soal:
 Hitung total jam kerja setiap karyawan dalam format XX jam YY menit.
 
-SELECT 
-    NamaKaryawan,
-    CONCAT(
-        DATEDIFF(MINUTE, JamMasuk, JamKeluar) / 60, ' jam ', 
-        DATEDIFF(MINUTE, JamMasuk, JamKeluar) % 60, ' menit'
-    ) AS DurasiKerja
-FROM Presensi
-WHERE DATEDIFF(MINUTE, JamMasuk, JamKeluar) > 480;
 
 
 

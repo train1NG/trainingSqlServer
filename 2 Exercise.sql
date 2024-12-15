371c77f75378a564a56d9ebc0fefbbbee35c0214
train1NG <tentiparfeb14@gmail.com>
2024-12-15 19:22:01 +0700

    2. Pattern match in query database SQL Server

---
+++ 2 Exercise.sql
@@ -0,0 +1,11 @@
+1. Cari nama belakang "Doe":
+2. Cari JobTitle yang mengandung "Developer":
+3. Cari email yang diakhiri "example.com":
+
+4. Temukan semua produk yang memiliki kata pertama "Laptop" di ProductName.
+5. Cari produk dengan nama yang dimulai dengan huruf "S".
+6. Temukan produk dengan harga di atas 1000 dan nama mengandung "Pro".
+
+7. Cari pelanggan yang nama depannya dimulai dengan huruf "A" menggunakan LEFT.
+8. Cari pelanggan yang memiliki titik (.) dalam alamat email menggunakan CHARINDEX.
+9. Cari pelanggan yang nomor teleponnya berisi "1234" menggunakan PATINDEX.

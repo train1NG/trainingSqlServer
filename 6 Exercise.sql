b939827336970f00417afad91f9eb9596f8d2c0b
train1NG <tentiparfeb14@gmail.com>
2024-12-15 20:39:03 +0700

    6. Transaction and ACID concept

---
+++ 6 Exercise.sql
@@ -0,0 +1,6 @@
+Soal 
+1.Pencarian Kamar yang Tersedia Tampilkan semua kamar yang saat ini tersedia untuk dipesan.
+2.Laporan Pemesanan Per Pelanggan Tampilkan laporan daftar pemesanan untuk setiap pelanggan, termasuk nama pelanggan, jenis kamar, tanggal check-in, dan tanggal check-out.
+3.Validasi Ketersediaan Kamar Sebelum Pemesanan Tulis query untuk memeriksa apakah kamar tertentu tersedia pada rentang tanggal tertentu.
+
+

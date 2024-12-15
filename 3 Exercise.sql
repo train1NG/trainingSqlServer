9e6fb05ce80265e9631909efdbeaf5f25b6e6f36
train1NG <tentiparfeb14@gmail.com>
2024-12-15 19:45:11 +0700

    3. Subquery and conditional expression

---
+++ 3 Exercise.sql
@@ -0,0 +1,15 @@
+1. Subquery: Tampilkan nama produk dan kategori untuk produk yang memiliki harga lebih tinggi daripada rata-rata harga semua produk.
+2. Conditional Expression: Tambahkan kolom Ketersediaan (Virtual Column) untuk menampilkan status produk:
+Tersedia jika stok lebih dari 10.
+Habis jika stok 0.
+Terbatas untuk stok antara 1 hingga 10.
+
+3. Subquery dan Join: Tampilkan nama produk, kategori, jumlah terjual untuk semua transaksi.
+4. Analisis Conditional Expression: Hitung total pendapatan per kategori. 
+5. CTE: Buat query untuk menghitung total stok per kategori menggunakan CTE.
+6. CTE dan Join: Gunakan CTE untuk menghitung penjualan per produk, lalu gabungkan dengan tabel produk untuk menampilkan nama produk, kategori.
+
+
+
+
+

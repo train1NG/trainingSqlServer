1faabe9688beae44b75699d4c83e9b0f207fa400
train1NG <tentiparfeb14@gmail.com>
2024-12-16 03:32:08 +0700

    7. Database locking

---
+++ 7 Exercise.sql
@@ -0,0 +1,10 @@
+1.Tampilkan semua data dalam tabel Orders menggunakan Shared Lock.
+2. Ubah status pesanan OrderID = 1 menjadi Processed menggunakan Exclusive Lock.
+3. Simulasikan penggunaan Update Lock saat membaca data yang akan diperbarui.
+
+
+4. Gunakan Read Uncommitted untuk menampilkan semua pesanan.
+5. Gunakan Read Committed untuk menampilkan data yang telah dikomit.
+6. Simulasikan Serializable untuk mencegah semua transaksi lain mengakses tabel.
+
+

eb8b39c2ec5389d711ce5faed81db668f480d832
train1NG <tentiparfeb14@gmail.com>
2024-12-17 04:39:13 +0700

    Add files via upload

---
+++ 12 Exercise.sql
@@ -0,0 +1,18 @@
+Tugas:
+
+1. Lakukan query berikut dan bandingkan waktu eksekusi sebelum dan sesudah indeks:
+
+2. Buat Clustered Index pada kolom OrderID
+
+3. Buat Non-Clustered Index pada kolom LastName
+
+4. Eksekusi query berikut untuk mengamati peningkatan performa:
+
+5. Bandingkan waktu eksekusi query:
+
+6. Buat Filtered Index untuk kolom Status dengan nilai Active:
+
+7. Buat Full-Text Index pada kolom Description
+
+8. Lakukan pencarian teks:
+
